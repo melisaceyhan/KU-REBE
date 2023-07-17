6c432d8077314ec08f8de782e11a0e9cd00e3663
ozanyilmaz891 <ozanyilmaz742@gmail.com>
2023-07-17 19:23:14 +0300

    SkorSistemi

--- Assets/ScoreManagerScript.cs
+++ Assets/ScoreManagerScript.cs
@@ -8,9 +8,27 @@ public class ScoreManagerScript : MonoBehaviour
     public int PlayerScore;
     public Text PlayerScoretext;
     
-     public void IncreaseScore()
+    public void IncreaseScore1()
     {
-        PlayerScore += 5;
+        PlayerScore += 30;
+        PlayerScoretext.text = PlayerScore.ToString();
+    }
+
+    public void IncreaseScore2()
+    {
+        PlayerScore += 50;
+        PlayerScoretext.text = PlayerScore.ToString();
+    }
+
+    public void IncreaseScore3()
+    {
+        PlayerScore += 100;
+        PlayerScoretext.text = PlayerScore.ToString();
+    }
+
+    public void IncreaseScore4()
+    {
+        PlayerScore += 200;
         PlayerScoretext.text = PlayerScore.ToString();
     }
 }
