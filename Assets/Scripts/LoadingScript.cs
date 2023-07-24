46adf12c6644b64b9eafdbf17c70805f08f7cb9f
Melisa <melisaceyhann@gmail.com>
2023-07-24 16:22:04 +0300

    loading

--- Assets/Scripts/LoadingScript.cs
+++ Assets/Scripts/LoadingScript.cs
@@ -1,32 +1,40 @@
+using JetBrains.Annotations;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LoadingScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    
+    public string sceneName;
     void Start()
     {
+
         this.GetComponent<TextMeshProUGUI>().text = "Loading..";
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        this.GetComponent<TextMeshProUGUI>().text += ".";
+        // this.GetComponent<TextMeshProUGUI>().text += ".";
+        WaitAndPrint(Time.deltaTime);
     }
-    private IEnumerator WaitAndPrint (float waitTime)
+    private IEnumerator WaitAndPrint(float waitTime)
     {
-        
-        GetComponent<TextMeshProUGUI>().text += ".";
+
+        GetComponent<TextMeshProUGUI>().text = "Loading.";
         yield return new WaitForSeconds(0.2f);
 
-       // GetComponent<TextMeshProUGUI>().text -= ".";
+        GetComponent<TextMeshProUGUI>().text = "Loading..";
         yield return new WaitForSeconds(0.2f);
 
-        
+
     }
 }
+
+
