2cd8f7c184386f72239fabb07d78f761920b0201
ozanyilmaz891 <ozanyilmaz742@gmail.com>
2023-07-28 18:19:32 +0300

    Leaderboard

--- Assets/ScoreAdderScript.cs
+++ Assets/ScoreAdderScript.cs
@@ -2,19 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class ScoreAdderScript : MonoBehaviour
 {
+        public TMP_Text scoreText;
+        private int ScoreDisplay = 0;
 
     void Start()
     {
+        if (ScoreManagerScript.PlayerScore < PlayerPrefs.GetInt("PlayerScore"))
+        {
+            ScoreDisplay = PlayerPrefs.GetInt("PlayerScore");
+        }
+
+        else if (ScoreManagerScript.PlayerScore >= PlayerPrefs.GetInt("PlayerScore"))
+        {
+            ScoreDisplay = ScoreManagerScript.PlayerScore;
+        }
+
+        scoreText.text = ScoreDisplay.ToString();
+
         SendScore();
     }
 
     public void SendScore()
     {
 
-        HighScores.UploadScore("ozan", 100);
+        HighScores.UploadScore("ozank", ScoreDisplay);
     }
 
 }
